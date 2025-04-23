0986414fea52cf61e559618e06bab1482819223d
denis-pdsb <denisk@podscribe.com>
2025-04-24 01:41:09 +0300

    [APP]: add health end-point

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ZoningController } from './zoning/zoning.controller';
 import { ZoningService } from './zoning/zoning.service';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [],
-  controllers: [ZoningController],
+  controllers: [ZoningController, HealthController],
   providers: [ZoningService],
 })
 export class AppModule {}
